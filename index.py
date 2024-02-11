b6251dc330fcd904e88327844506192d6f711fd3
Aizawa Tomo <diazpenajuansebastian24@gmail.com>
2024-02-11 16:58:44 -0500

    Agregando la opción de que el usuario eliga nombre, y la máquina igual

--- index.py
+++ index.py
@@ -1,24 +1,36 @@
 import random
 
-valNumerico = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "AS"]
-valSimbolo = [" ♣", " ♥", " ♠", " ♦"]
-
 def cartaRandom():
     cartaAleatoria = random.choice(range(len(cartasFormadas)))
     cartasFormadas.pop(cartaAleatoria) 
     # carta = print(f"Carta: {cartasFormadas[cartaAleatoria]}")
     return cartasFormadas[cartaAleatoria]
 
-def mostrarMazo():
+def mostrarMazoUsu():
     for mazo in mazoJugador:
         print(mazo)
+
+def mostrarMazoPc():
+    for mazo in mazoCompu:
+        print(mazo)
+
+def nombrePC():
+    nombreAleatorioPc = random.choice(range(len(nombresPCArray)))    
+    return nombresPCArray[nombreAleatorioPc]
+
+nombreUsuario = input("\n• Nombre del usuario: ")
+nombresPCArray = ["Itadori" , "Naruto" , "Moa-metal" , "Momo-metal" , "Su-metal"]
+
+valNumerico = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "AS"]
+valSimbolo = ["♣", "♥", "♠", "♦"]
+
+nombrePCPartida = nombrePC() # Eligiendo el nombre del pc
 
 cartasFormadas = [(num , sim) for num in valNumerico for sim in valSimbolo] #Lista por compresnsión (acá formamos todas las tarjetas)
 
 for carta in cartasFormadas:
     numero, simbolo = carta
     #print(f"{numero}{simbolo}") # Para que sea "2 ♥" en lugar de "(2, ' ♣')", usar la función f-string en Python. permite formatear cadenas de forma dinámica.
-
 
 mazoJugador = [] 
 mazoCompu = []
@@ -32,6 +44,10 @@ for contador in range(4):
 
     contador += 1
 
-print("• Cartas en el mazo del usuario:")
+print("\n• Cartas en el mazo de" , nombreUsuario , ": ")
+mostrarMazoUsu()
 
-mostrarMazo()
+print("\n• El mazo del", nombrePCPartida ,"se podrá ver al final de las partida")
+
+# print("\n• Cartas en el mazo del computador:")
+# mostrarMazoPc()
