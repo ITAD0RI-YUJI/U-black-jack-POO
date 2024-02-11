20ab6ec3602587b5111f43c91dd1c8629033708f
Aizawa Tomo <diazpenajuansebastian24@gmail.com>
2024-02-11 12:47:10 -0500

    Generando las dos cartas aleatorias para el usuario

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+import random
+
 valNumerico = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "AS"]
 valSimbolo = [" ♣", " ♥", " ♠", " ♦"]
 
@@ -5,5 +7,14 @@ cartasFormadas = [(num , sim) for num in valNumerico for sim in valSimbolo] #Lis
 
 for carta in cartasFormadas:
     numero, simbolo = carta
-    print(f"{numero}{simbolo}") # Para que sea "2 ♥" en lugar de "(2, ' ♣')", usar la función f-string en Python. permite formatear cadenas de forma dinámica.
+    #print(f"{numero}{simbolo}") # Para que sea "2 ♥" en lugar de "(2, ' ♣')", usar la función f-string en Python. permite formatear cadenas de forma dinámica.
 
+
+mazoJugador = [] #colocar función para que escoja número random (posiciones) de las cartas ya formadas
+mazoCompu = []
+
+cartaAleatoriaUno = random.choice(range(len(cartasFormadas))) # el "len()" es como el .lenght en JS
+cartaAleatoriaDos = random.choice(range(len(cartasFormadas))) 
+
+print(f"Carta: {cartasFormadas[cartaAleatoriaUno]}")
+print(f"Carta: {cartasFormadas[cartaAleatoriaDos]}")
