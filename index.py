b5955f833b29396f129c8225cb88841270e95624
Aizawa Tomo <diazpenajuansebastian24@gmail.com>
2024-02-11 16:37:24 -0500

    Agregando al proceso para mostrar el mazo del usuario

--- index.py
+++ index.py
@@ -4,9 +4,14 @@ valNumerico = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "AS"]
 valSimbolo = [" ♣", " ♥", " ♠", " ♦"]
 
 def cartaRandom():
-    cartaAleatoria = random.choice(range(len(cartasFormadas))) 
-    carta = print(f"Carta: {cartasFormadas[cartaAleatoria]}")
-    return carta
+    cartaAleatoria = random.choice(range(len(cartasFormadas)))
+    cartasFormadas.pop(cartaAleatoria) 
+    # carta = print(f"Carta: {cartasFormadas[cartaAleatoria]}")
+    return cartasFormadas[cartaAleatoria]
+
+def mostrarMazo():
+    for mazo in mazoJugador:
+        print(mazo)
 
 cartasFormadas = [(num , sim) for num in valNumerico for sim in valSimbolo] #Lista por compresnsión (acá formamos todas las tarjetas)
 
@@ -15,8 +20,18 @@ for carta in cartasFormadas:
     #print(f"{numero}{simbolo}") # Para que sea "2 ♥" en lugar de "(2, ' ♣')", usar la función f-string en Python. permite formatear cadenas de forma dinámica.
 
 
-mazoJugador = [] #colocar función para que escoja número random (posiciones) de las cartas ya formadas
+mazoJugador = [] 
 mazoCompu = []
 
-carta = cartaRandom()
-carta = cartaRandom()
+for contador in range(4):
+    cartaSelec = cartaRandom()
+    if(contador < 2):
+        mazoJugador.append(cartaSelec)
+    else:
+        mazoCompu.append(cartaSelec)
+
+    contador += 1
+
+print("• Cartas en el mazo del usuario:")
+
+mostrarMazo()
